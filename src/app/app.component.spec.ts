390659f7a026c47a2715da090c2955aa442e051a
Cristian Martinez <dev.cmartinez@gmail.com>
2021-01-17 21:36:51 +0100

    test: add blog service testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'ng-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-demo');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-demo app is running!');
-  });
 });
--- src/app/blog/services/blog.service.spec.ts
+++ src/app/blog/services/blog.service.spec.ts
@@ -1,16 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
-import { BlogService } from './blog.service';
+import {BlogService, Post} from './blog.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('BlogService', () => {
   let service: BlogService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BlogService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    httpController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  beforeEach(inject(
+    [BlogService],
+    (it: BlogService) => {
+      service = it;
+    }
+  ));
+
+  it('should return post data', () => {
+    let result: Post[];
+    service.getLatestPosts().subscribe((data) => result = data);
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: `${BlogService.API_URL}/posts`
+    });
+
+    const post1: Post = {
+      id: 1,
+      userId: 2,
+      title: 'Title 1',
+      body: 'Body 1'
+    };
+
+    const post2: Post = {
+      id: 2,
+      userId: 2,
+      title: 'Title 2',
+      body: 'Body 2'
+    };
+
+    req.flush([post1, post2]);
+
+    expect(result.length).toEqual(2);
+    expect(result[0]).toEqual(post1);
+    expect(result[1]).toEqual(post2);
   });
 });
